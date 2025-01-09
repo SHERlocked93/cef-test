63711783d5e3feab8107a43d40ae96602cc77ae3
SHERlocked93 <443033099@qq.com>
2025-01-09 18:52:25 +0800

    feat: 可以自定义alert、confirm对话框

--- be-src/1_PresentationCore/ViewDelegate/PageHandler.cpp
+++ be-src/1_PresentationCore/ViewDelegate/PageHandler.cpp
@@ -2,6 +2,38 @@
 
 #include "include/wrapper/cef_helpers.h"
 
+bool PageHandler::OnBeforeUnloadDialog(CefRefPtr<CefBrowser> browser, const CefString& message_text,
+                                       bool is_reload, CefRefPtr<CefJSDialogCallback> callback) {
+  // message_text.c_str() 的值是Is it OK to leave/refresh this page
+  int msgboxID = MessageBox(NULL, L"您编辑的内容尚未保存.\n确定要关闭窗口吗?", L"系统提示",
+                            MB_ICONEXCLAMATION | MB_OKCANCEL);
+  if (msgboxID == IDOK) {
+    callback->Continue(true, CefString());
+  } else {
+    callback->Continue(false, CefString());
+  }
+  return true;
+}
+
+bool PageHandler::OnJSDialog(CefRefPtr<CefBrowser> browser, const CefString& origin_url,
+                             JSDialogType dialog_type, const CefString& message_text,
+                             const CefString& default_prompt_text, CefRefPtr<CefJSDialogCallback> callback,
+                             bool& suppress_message) {
+  suppress_message = false;
+  HWND hwnd = browser->GetHost()->GetWindowHandle();
+  if (dialog_type == JSDialogType::JSDIALOGTYPE_ALERT) {  // alert弹框
+    MessageBox(hwnd, message_text.ToWString().c_str(), L"系统提示alert", MB_ICONEXCLAMATION | MB_OK);
+    callback->Continue(true, CefString());
+  } else if (dialog_type == JSDialogType::JSDIALOGTYPE_CONFIRM) {  // confirm弹框
+    int msgboxID =
+        MessageBox(hwnd, message_text.ToWString().c_str(), L"系统提示confirm", MB_ICONEXCLAMATION | MB_YESNO);
+    callback->Continue(msgboxID == IDYES, CefString());
+  } else if (dialog_type == JSDialogType::JSDIALOGTYPE_PROMPT) {  // prompt弹框
+    return false;
+  }
+  return true;
+}
+
 void PageHandler::OnAfterCreated(CefRefPtr<CefBrowser> browser) {
   CEF_REQUIRE_UI_THREAD()
   browsers.push_back(browser);
--- be-src/1_PresentationCore/ViewDelegate/PageHandler.h
+++ be-src/1_PresentationCore/ViewDelegate/PageHandler.h
@@ -2,9 +2,34 @@
 #include <list>
 
 #include "include/cef_app.h"
-class PageHandler : public CefClient, public CefLifeSpanHandler {
+class PageHandler : public CefClient, public CefLifeSpanHandler, public CefJSDialogHandler {
  public:
   PageHandler() = default;
+
+  CefRefPtr<CefJSDialogHandler> GetJSDialogHandler() override { return this; }
+
+  //! 阻止页面关闭的对话框弹出之前触发的事件
+  //! \param browser 浏览器对象
+  //! \param message_text 提示文本字符串
+  //! \param is_reload 当前关闭页面的行为是不是刷新行为
+  //! \param callback
+  //! \return
+  bool OnBeforeUnloadDialog(CefRefPtr<CefBrowser> browser, const CefString& message_text, bool is_reload,
+                            CefRefPtr<CefJSDialogCallback> callback) override;
+
+  //! 弹出对话框事件
+  //! \param browser 浏览器对象
+  //! \param origin_url 弹出对话框的页面的url
+  //! \param dialog_type 弹出对话框的类型 alert、confirm、prompt
+  //! \param message_text JS提供的弹出对话框所承载的文本信息
+  //! \param default_prompt_text 显示在prompt对话框中输入框的默认值
+  //! \param callback 把用户做出的选择通知回调
+  //! \param suppress_message 要不要屏蔽默认对话框
+  //! \return
+  bool OnJSDialog(CefRefPtr<CefBrowser> browser, const CefString& origin_url, JSDialogType dialog_type,
+                  const CefString& message_text, const CefString& default_prompt_text,
+                  CefRefPtr<CefJSDialogCallback> callback, bool& suppress_message) override;
+
   //! 用于处理页面生命周期中的事件
   CefRefPtr<CefLifeSpanHandler> GetLifeSpanHandler() override { return this; }
   //! 页面创建成功
